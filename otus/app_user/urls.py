739c34a94d453de40c6b2591614df7cabe1800d4
Svistov Nikolai <swistov@gmail.com>
2019-06-12 23:24:39 +0300

    - Add user create and auth

---
+++ otus/app_user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from app_user import views
+
+app_name = 'app_user'
+
+urlpatterns = [
+    path('api/create/', views.UserCreateView.as_view(), name='create'),
+    path('api/login/', views.UserLoginView.as_view(), name='login'),
+]
---
+++ otus/app_user/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from app_user import serializers
+
+
+class UserCreateView(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = serializers.UserSerializer
+
+
+class UserLoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            return Response({'token': user.auth_token.key})
+        else:
+            return Response({'error': 'Wrong Credentials'}, status=status.HTTP_400_BAD_REQUEST)
